e6da2d2cf67dbb566b723a4d3f70f58b5466f0db
nandhana k <nandhanababu508@gmail.com>
2024-04-23 13:53:37 +0530

    wed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { useState } from 'react';
+
+import { Button,StyleButton,StyledButton,Box } from './Components/Style';
+
+function App() {
+
+  const [couter,setcouter]=useState(0);
+
+
+  return (
+    <div>
+      <Box>
+      <h2 style={{marginLeft:"150px"}}>Couter testing</h2>
+      <h4 style={{marginLeft:"220px"}}>{couter}</h4>
+      <div>
+      <StyleButton onClick={()=>{setcouter(couter+1)}}>Increment</StyleButton>
+      <StyledButton onClick={()=>{setcouter(couter-1)}}disabled={couter===0}>Decrement</StyledButton>
+      
+     
+     <Button onClick={()=>{setcouter(0)}}>Reset</Button>
+     </div>
+     </Box>
+    </div>
+  );
+}
+
+export default App;
